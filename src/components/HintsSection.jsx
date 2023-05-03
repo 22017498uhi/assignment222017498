637e1f39035b7b1842567b3158e3fcd553bfde21
22017498uhi <113307467+22017498uhi@users.noreply.github.com>
2023-05-03 12:16:57 +0100

    updated site title and favicon

--- src/components/HintsSection.jsx
+++ src/components/HintsSection.jsx
@@ -20,13 +20,15 @@ function HintsSection() {
 
     const [hintsData, setHintsData] = useState("")
 
-   
+
     const [summaryImageUrl, setSummaryImageUrl] = useState("")
     const [videoUrl, setVideoUrl] = useState("")
 
 
     const [summaryPopupOpen, setSummaryPopupOpen] = useState(false);
     const [videoPopupOpen, setVideoPopupOpen] = useState(false);
+
+    const [showConfusedForm, setShowConfusedForm] = useState(false);
 
 
     const closeSummaryModal = () => setSummaryPopupOpen(false);
@@ -90,24 +92,24 @@ function HintsSection() {
                                         {videoObj?.type == columnObj.linkTitle && videoObj?.link == rowObj.linkTitle
                                             && (
                                                 <div className="d-flex flex-column" style={{ border: `2px solid ${videoObj?.type === 'general' ? '#0d6efcb8' : '#1987549c'}`, padding: '4px 4px 2px 4px', borderRadius: '5px' }}>
-                                                    <button type="button" name={videoObj.title} 
-                                                    className={`btn mb-1 p-2 ${videoObj?.type === 'general' ? 'btn-primary' : 'btn-success'}`}
-                                                    onClick={() => {
-                                                        getDownloadURL(ref(storage, `quesvideos/${videoObj.title}.mp4`))
-                                                            .then((url) => {
-                                                                setVideoUrl(url);
-                                                                setVideoPopupOpen(o => !o)
-                                                            });
-                                                    }}>VIDEO</button>
-                                                    
-                                                    <button type="button" name={videoObj.title} 
-                                                    className={`btn mb-1 p-2 ${videoObj?.type === 'general' ? 'btn-primary' : 'btn-success'}`}
+                                                    <button type="button" name={videoObj.title}
+                                                        className={`btn mb-1 p-2 ${videoObj?.type === 'general' ? 'btn-primary' : 'btn-success'}`}
+                                                        onClick={() => {
+                                                            getDownloadURL(ref(storage, `quesvideos/${videoObj.title}.mp4`))
+                                                                .then((url) => {
+                                                                    setVideoUrl(url);
+                                                                    setVideoPopupOpen(o => !o)
+                                                                });
+                                                        }}>VIDEO</button>
+
+                                                    <button type="button" name={videoObj.title}
+                                                        className={`btn mb-1 p-2 ${videoObj?.type === 'general' ? 'btn-primary' : 'btn-success'}`}
                                                         onClick={() => {
                                                             getDownloadURL(ref(storage, `quesimages/${videoObj.title}.jpg`))
-                                                            .then((url) => {
-                                                                setSummaryImageUrl(url);
-                                                                setSummaryPopupOpen(o => !o)
-                                                            });
+                                                                .then((url) => {
+                                                                    setSummaryImageUrl(url);
+                                                                    setSummaryPopupOpen(o => !o)
+                                                                });
                                                         }}>SUMMARY</button>
                                                 </div>
                                             )}
@@ -130,7 +132,7 @@ function HintsSection() {
             </div>
 
             {/* Summary Button Popup code is below */}
-            <Popup open={summaryPopupOpen} onClose={closeSummaryModal}>
+            <Popup lockScroll repositionOnResize  open={summaryPopupOpen}  onClose={closeSummaryModal}>
                 <div className="my-modal">
                     <a className="close" onClick={closeSummaryModal}>
                         &times;
@@ -140,12 +142,30 @@ function HintsSection() {
             </Popup>
 
             {/* Video Button Popup code is below */}
-            <Popup open={videoPopupOpen} onClose={closeVideoModal}>
-                <div className="my-modal">
+            <Popup lockScroll repositionOnResize open={videoPopupOpen}  onClose={closeVideoModal}>
+                <div className="my-modal my-2">
                     <a className="close" onClick={closeVideoModal}>
                         &times;
                     </a>
-                    <span><ReactPlayer url={videoUrl} playing="true"   width={640} height={480} controls="true" /></span>
+                    <div><ReactPlayer url={videoUrl} playing="true" width={640} height="50vh" controls="true" /></div>
+                    <div className='d-flex justify-content-end my-2 mx-3'>
+                        { !showConfusedForm && <button className='btn btn-danger' onClick={() => {
+                            setShowConfusedForm(true)
+                        }}>Confused?</button> }
+                    </div>
+
+                    {/* Confused question posting stuff below */}
+                    {showConfusedForm &&<div className='mx-3'>
+                        <div>What have you found confusing about this video?</div>
+                        <textarea class="form-control" style={{borderColor:'grey'}} id="videoquerytext" rows="2"></textarea>
+
+                         <div className='mt-3'>
+                            <button className='btn btn-primary me-3'>Send</button>
+                            <button className='btn btn-outline-dark' onClick={() => {
+                                setShowConfusedForm(false)
+                            }}>Cancel</button>
+                        </div>
+                    </div>}
                 </div>
             </Popup>
 
